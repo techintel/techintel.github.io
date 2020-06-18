9a5ec09f6a964b8469720f377768723f3a376894
5ervant Mark Anthony B. Dungo <5ervant@protonmail.com>
2020-06-18 22:54:44 +0800

    New info post created

--- _next/static/runtime/webpack.js
+++ _next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "bdc3963f41a6dbc5eba9";
+/******/ 	var hotCurrentHash = "6e8e67cf75c531e5aae6";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
