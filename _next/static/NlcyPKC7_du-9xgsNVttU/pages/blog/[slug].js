61a84a4fc2ce204c323761b63f4f7edb27784681
5ervant Mark Anthony B. Dungo <5ervant@protonmail.com>
2020-07-04 05:08:01 +0800

    Updated info post

---
+++ _next/static/NlcyPKC7_du-9xgsNVttU/pages/blog/[slug].js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[17],{KTQB:function(n,o,w){"use strict";w.r(o);var u=w("TWYl");o.default=u.a},ypH6:function(n,o,w){(window.__NEXT_P=window.__NEXT_P||[]).push(["/blog/[slug]",function(){return w("KTQB")}])}},[["ypH6",0,1,6,2,3,5,4,7,8,10]]]);
