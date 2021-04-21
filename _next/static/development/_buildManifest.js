5312ea4a8903a48ec1dbbc2bbb621c8b24401d8c
5ervant | techintel.github.io <5ervant@users.noreply.github.com>
2021-04-21 20:19:34 +0800

    Updated info post

--- _next/static/development/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:[],"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/crypto-lands":["static\u002Fchunks\u002Fpages\u002Fcrypto-lands.js"],sortedPages:["\u002F_app","\u002F_error","\u002Fcrypto-lands"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
